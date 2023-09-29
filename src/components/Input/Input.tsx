620884a8f04dac93b38c711727bf898f54b7b709
mst-mkt <s1f102303057@iniad.org>
2023-09-29 11:29:51 +0900

    add: subscribe form

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,24 @@
+import type { ChangeEvent } from 'react';
+import styles from './Input.module.css';
+
+type Props = {
+  label: string | null;
+  name: string;
+  value: number;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input = (props: Props) => {
+  return (
+    <div className={styles.container}>
+      {props.label !== null && <label htmlFor={props.name}>{props.label}</label>}
+      <input
+        type="text"
+        name={props.name}
+        id={props.name}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </div>
+  );
+};
