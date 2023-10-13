5ce38d5659a99e6d0dafa68508601353e127d700
yu7400ki <yu7400ki@users.noreply.github.com>
2023-10-13 18:39:08 +0000

    update aspida

--- api/enter/:id/index.ts
+++ api/enter/:id/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable */
 import type * as Types from '../../@types';
 
 export type Methods = {
+  /** ユーザーを企画に参加させる */
   post: {
     status: 200;
+    /** 参加履歴 */
     resBody: Types.HistoryResponse;
   };
 };
--- api/user/index.ts
+++ api/user/index.ts
@@ -1,25 +1,34 @@
+/* eslint-disable */
 import type * as Types from '../@types';
 
 export type Methods = {
+  /** ユーザーの情報を取得する */
   get: {
     status: 200;
+    /** ユーザー情報 */
     resBody: Types.UserResponse;
   };
 
+  /** ユーザーを作成する */
   post: {
     status: 200;
+    /** 作成されたユーザー情報 */
     resBody: Types.UserResponse;
     reqBody: Types.UserRequest;
   };
 
+  /** ユーザー情報を更新する */
   put: {
     status: 200;
+    /** 更新されたユーザー情報 */
     resBody: Types.UserResponse;
     reqBody: Types.UserRequest;
   };
 
+  /** ユーザーを削除する */
   delete: {
     status: 200;
+    /** ユーザーが削除された */
     resBody: Types.UserResponse;
   };
 };
