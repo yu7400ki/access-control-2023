b4df7bd1fe5af1dc359504ac5931528bccb3cdd8
yu7400ki <yu7400ki@users.noreply.github.com>
2023-10-13 19:36:00 +0000

    update aspida

---
+++ api/program/:id/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import type * as Types from '../../@types';
+
+export type Methods = {
+  /** 企画を取得する */
+  get: {
+    status: 200;
+    /** 企画 */
+    resBody: Types.ProgramResponse;
+  };
+};
