331b6f431b2f5e83e87b57f56e6336b7d28f1832
yu7400ki <yu7400ki@users.noreply.github.com>
2023-10-14 09:20:43 +0000

    update aspida

--- api/history/index.ts
+++ api/history/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable */
 import type * as Types from '../@types';
 
 export type Methods = {
+  /** ユーザーが参加した企画一覧履歴を取得する */
   get: {
     status: 200;
+    /** ユーザーの参加履歴一覧 */
     resBody: Types.HistoryResponse;
   };
 };
